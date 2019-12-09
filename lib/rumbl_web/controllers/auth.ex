c0d763847d15028fa955f5a13e10fe3141e6224f
Pushkar Singh <theagilecoder@gmail.com>
2019-12-09 10:19:52 +0530

    Chapter 6 Create Video resource & refactor :authenticate_user

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -1,13 +1,11 @@
 defmodule RumblWeb.Auth do
   import Plug.Conn
 
-  alias Rumbl.Accounts
-
   def init(opts), do: opts
 
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
-    user = user_id && Accounts.get_user(user_id)
+    user = user_id && Rumbl.Accounts.get_user(user_id)
     assign(conn, :current_user, user)
   end
 
@@ -19,7 +17,7 @@ defmodule RumblWeb.Auth do
   end
 
   def login_by_email_and_pass(conn, email, given_pass) do
-    case Accounts.authenticate_by_email_and_pass(email, given_pass) do
+    case Rumbl.Accounts.authenticate_by_email_and_pass(email, given_pass) do
       {:ok, user} -> {:ok, login(conn, user)}
       {:error, :unauthorized} -> {:error, :unauthorized, conn}
       {:error, :not_found} -> {:error, :not_found, conn}
@@ -28,5 +26,19 @@ defmodule RumblWeb.Auth do
 
   def logout(conn) do
     configure_session(conn, drop: true)
+  end
+
+  import Phoenix.Controller
+  alias RumblWeb.Router.Helpers, as: Routes
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
   end
 end
